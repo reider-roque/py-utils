faeec64f012690f9131194010eb33acf283764fc
Oleg Mitrofanov <it@unlim.xyz>
2015-04-30 09:46:35 -0400

    Refactor message type logic

--- print_status.py
+++ print_status.py
@@ -3,33 +3,45 @@ import sys
 
 def print_status(message="", type="info"):
     color_print = sys.stdout.isatty() and platform.system() != "Windows"
-
-    if type = "success" or type = "good"
-        if color_print:
-            print("\033[1;32m[+]\033[1;m {}".format(message))
-        else:
-            print("[+] {}".format(message))
         
-    elif type == "fail" or type = "error":
-        if color_print:
-            print("\033[1;31m[-]\033[1;m {}".format(message))
-        else:
-            print("[-] {}".format(message))
-        
-    elif type == "info":
+    if type == "info":
         if color_print:
             print("\033[1;34m[*]\033[1;m {}".format(message))
         else:
             print("[*] {}".format(message))
         
-    elif type == "debug" or type == "warn":
+    elif type == "debug":
         if color_print:
-            print("\033[1;34m[!]\033[1;m {}".format(message))
+            print("\033[1;33m[?]\033[1;m {}".format(message))
         else:
-            print("[!] {}".format(message))
+            print("[?] {}".format(message))
         
-    elif type == "panic" or type = "alert":
+    elif type == "warn" or type == "error":
         if color_print:
             print("\033[1;31m[!]\033[1;m {}".format(message))
         else:
             print("[!] {}".format(message))
+        
+    elif type == "fail":
+        if color_print:
+            print("\033[1;31m[-]\033[1;m {}".format(message))
+        else:
+            print("[-] {}".format(message))
+
+    elif type == "maybe":
+        if color_print:
+            print("\033[1;32m[~]\033[1;m {}".format(message))
+        else:
+            print("[~] {}".format(message))
+
+    elif type == "success":
+        if color_print:
+            print("\033[1;32m[+]\033[1;m {}".format(message))
+        else:
+            print("[+] {}".format(message))
+        
+    elif type == "alert" or type == "panic":
+        if color_print:
+            print("\033[1;31m[ALERT]\033[1;m {}".format(message))
+        else:
+            print("[ALERT] {}".format(message))
---
+++ print_status_test.py
@@ -0,0 +1,9 @@
+from print_status import print_status
+
+print_status("Info", "info")
+print_status("Debug", "debug")
+print_status("Warn", "warn")
+print_status("Fail", "fail")
+print_status("Maybe", "maybe")
+print_status("Success", "success")
+print_status("Critical", "alert")
